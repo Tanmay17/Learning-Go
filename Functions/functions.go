1ee28bd4167863f118557ba03c4374e2861e15d8
Tanmay Srivastava <tanmay@getinfino.com>
2020-08-06 19:54:23 +0530

    feat: learned about variadic function

--- Functions/functions.go
+++ Functions/functions.go
@@ -14,11 +14,22 @@ func multipleReturn(a string, b int, c int) (int, string) {
 
 }
 
+func variadicFunc(nums ...int) {
+
+	for _, num := range nums {
+		fmt.Println(num)
+	}
+
+}
+
 func main() {
 
 	sum, msg := multipleReturn("Test Message", 13, 12)
 	fmt.Println("Sum1:", sum)
 	fmt.Println("Msg:", msg)
 	fmt.Println("Sum2:", add(12, 12))
+
+	nums := []int{1, 2, 3, 4}
+	variadicFunc(nums...)
 
 }
