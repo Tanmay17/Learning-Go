f897952410806b35d6aa49db332b6bb26f3727fd
Tanmay Srivastava <tanmay@getinfino.com>
2020-08-06 14:10:05 +0530

    feat: Learning about declaring variables

---
+++ Variables/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a = 1
+	fmt.Println(a)
+	var b, c, d string = "1", "2", "Suppp... Duniya!!!"
+	fmt.Println(c+b, d)
+
+	e := true
+	fmt.Println(e)
+}
