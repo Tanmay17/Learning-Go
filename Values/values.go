6976483f7769b045bcd6a8e280ccd327f1238788
Tanmay Srivastava <tanmay@getinfino.com>
2020-08-06 13:36:19 +0530

    feat: Learning about types of values

---
+++ Values/values.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println( "go" + "lang" )
+
+	fmt.Println( "1+1= ", 1+1 )
+	fmt.Println( "10.0+1.35= ", 10.0+1.35 )
+
+	fmt.Println( true && false )
+	fmt.Println( true || false )
+	fmt.Println( !false )
+
+}
